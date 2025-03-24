6b06afe0d9901792374d3d557f33c2ba30390f95
FABIO MONTEIRO AMORIM <fmamorim_ext@grupogbi.com>
2025-03-24 11:42:06 -0300

    finalizando a dockerização

--- src/main/java/br/gov/cnpq/controller/PocController.java
+++ src/main/java/br/gov/cnpq/controller/PocController.java
@@ -2,11 +2,14 @@ package br.gov.cnpq.controller;
 
 
 import br.gov.cnpq.json.Response;
+import br.gov.cnpq.service.PocService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -15,12 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/v1/poc")
 public class PocController {
 
+    @Autowired
+    private PocService pocService;
+
     @GetMapping(value = "/teste")
-    public ResponseEntity<Response> realizarTeste(){
+    public ResponseEntity<Response> realizarTeste(@RequestParam(value = "mensagem", required = true) String mensagem){
         Response response = new Response();
         try {
             response.setModeloRetorno(null);
             response.setMensagensRetorno("Micorservice esta no ar" );
+            pocService.send(mensagem);
 
         }catch (Exception e){
             log.error("Erro ao testar a poc: " + e.getMessage());
